4bfc9303ff54cf698b6357dfd05df3a9d332b921
SandraReichel <sandra.reichel.dresden@gmail.com>
2021-02-28 19:58:32 +0100

    double padding

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -19,14 +19,18 @@ import android.os.Bundle
 import android.text.Layout
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -37,6 +41,7 @@ import androidx.compose.ui.unit.dp
 import com.example.androiddevchallenge.data.Cat
 import com.example.androiddevchallenge.data.Feature
 import com.example.androiddevchallenge.ui.theme.MyTheme
+import com.example.androiddevchallenge.ui.theme.purple200
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -70,33 +75,58 @@ fun MyApp() {
 }
 
 @Composable
+fun Header() {
+    Text(text = "Ready... Set... GO!")
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
 fun CatList(cats: List<Cat>) {
+
     Column {
+        Header()
+
+
         cats.forEach { message ->
-            CatItem(message)
+            CatItem(message, {})
         }
     }
+
 }
 
 
 @Composable
-fun CatItem(cat: Cat) {
-    Card(elevation = 4.dp) {
-    Row(verticalAlignment = CenterVertically) {
-        Image(
-            painter = painterResource(R.drawable.karli),
-            contentDescription = "cute cat",
-            modifier = Modifier
-                .height(50.dp)
-                .width(50.dp)
-                .clip(shape = RoundedCornerShape(4.dp)),
-            contentScale = ContentScale.Crop
-        )
-        Column {
-            Text(cat.name)
-            Text(cat.age.toString())
+fun CatItem(cat: Cat, onClick: () -> Unit) {
+    Box(
+        modifier = Modifier
+            .clickable(onClick = onClick)
+            .padding(32.dp)
+            .fillMaxWidth()
+    ) {
+        Card(
+            elevation = 4.dp, backgroundColor = purple200, modifier = Modifier
+                .clickable(onClick = onClick)
+                .padding(32.dp)
+                .fillMaxWidth()
+        ) {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+
+
+                Image(
+                    painter = painterResource(R.drawable.karli),
+                    contentDescription = "cute cat",
+                    modifier = Modifier
+                        .height(50.dp)
+                        .width(50.dp)
+                        .clip(shape = RoundedCornerShape(4.dp)),
+                    contentScale = ContentScale.Crop
+                )
+                Column {
+                    Text(cat.name)
+                    Text(cat.age.toString())
+                }
+            }
         }
-    }
     }
 }
 

c30334fa45e2e1efc65da6155799263a954f1c8c
SandraReichel <sandra.reichel.dresden@gmail.com>
2021-03-02 23:16:50 +0100

    Revert "ise svg icons"
    This reverts commit fe362c902ec7eeb8258a7b42087957d29c9648ee.

--- app/src/main/java/com/example/androiddevchallenge/data/DataProvider.kt
+++ app/src/main/java/com/example/androiddevchallenge/data/DataProvider.kt
@@ -21,42 +21,42 @@ object DataProvider {
 
     fun getCatList() = arrayListOf(
         Cat(
-            1, "Karli", R.drawable.ic_baseline_pets_24, 17, Gender.Male,
+            1, "Karli", R.drawable.karli, 17, Gender.Male,
             arrayListOf(Fact.HomeTown("Spremberg"), Fact.ArrivalDate("12.12.2020")),
             arrayListOf(Feature.Fluffy, Feature.Cute)
         ),
         Cat(
-            2, "Moni", R.drawable.ic_baseline_pets_24, 5, Gender.Female,
+            2, "Moni", R.drawable.cat1, 5, Gender.Female,
             arrayListOf(Fact.HomeTown("Schwarze Pumpe"), Fact.ArrivalDate("08.09.2019")),
             arrayListOf(Feature.Playful, Feature.Cute)
         ),
         Cat(
-            3, "Mr Fluffels", R.drawable.ic_baseline_pets_24, 1, Gender.NotKnown,
+            3, "Mr Fluffels", R.drawable.cat2, 1, Gender.NotKnown,
             arrayListOf(Fact.HomeTown("Dresden"), Fact.ArrivalDate("01.01.2020")),
             arrayListOf(Feature.Fluffy, Feature.Cute, Feature.Quiet, Feature.Aggressive)
         ),
         Cat(
-            4, "Lucky", R.drawable.ic_baseline_pets_24, 9, Gender.Male,
+            4, "Lucky", R.drawable.cat3, 9, Gender.Male,
             arrayListOf(Fact.HomeTown("Hoyerswerda"), Fact.ArrivalDate("03.12.2020")),
             arrayListOf(Feature.Playful, Feature.Cute)
         ),
         Cat(
-            5, "Kuh", R.drawable.ic_baseline_pets_24, 11, Gender.Male,
+            5, "Kuh", R.drawable.cat4, 11, Gender.Male,
             arrayListOf(Fact.HomeTown("Heinrichsfeld"), Fact.ArrivalDate("02.03.2018")),
             arrayListOf()
         ),
         Cat(
-            6, "Purzel", R.drawable.ic_baseline_pets_24, 3, Gender.Female,
+            6, "Purzel", R.drawable.cat5, 3, Gender.Female,
             arrayListOf(Fact.HomeTown("Spremberg"), Fact.ArrivalDate("07.06.2018")),
             arrayListOf(Feature.Playful, Feature.Cute, Feature.Outside)
         ),
         Cat(
-            7, "Biene", R.drawable.ic_baseline_pets_24, 15, Gender.Female,
+            7, "Biene", R.drawable.cat2, 15, Gender.Female,
             arrayListOf(Fact.HomeTown("Spremberg"), Fact.ArrivalDate("01.02.2021")),
             arrayListOf(Feature.Fluffy, Feature.Quiet)
         ),
         Cat(
-            8, "Susi", R.drawable.ic_baseline_pets_24, 4, Gender.Male,
+            8, "Susi", R.drawable.karli, 4, Gender.Male,
             arrayListOf(Fact.HomeTown("Spremberg"), Fact.ArrivalDate("28.12.2021")),
             arrayListOf(Feature.Cute, Feature.Quiet, Feature.Aggressive)
         )
